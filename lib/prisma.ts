d7911ee244311644b86cdb457f30f6aea7ebba85
Sammarth Gupta <102611901+Samarth622@users.noreply.github.com>
2026-02-09 12:15:03 +0530

    Update Prisma client setup with adapter
    Refactor Prisma client initialization to use adapter.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
